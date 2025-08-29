427fe465f0cc8c858d6950cac1d7428a0c4c66af
Milton Rosenbaum <milton.rosenbaum@gmail.com>
2025-08-29 11:54:49 -0400

    first commit!

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                'resources/sass/app.scss',
+                'resources/js/app.js',
+                'resources/css/style.css',
+            ],
+            refresh: true,
+        }),
+        react(),
+        tailwindcss(),
+    ],
+});
